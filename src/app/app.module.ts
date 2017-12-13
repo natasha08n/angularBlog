752ba4ea7fd1a842e3baee1fba7ad02edf7a7fcd
Natallia Chobat <natallia.chobat@itechart-group.com>
2017-12-13 22:04:57 +0300

    Add new imports for material-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
-import { BrowserModule }   from '@angular/platform-browser';
-import { NgModule }        from '@angular/core';
+import { BrowserModule }                   from '@angular/platform-browser';
+import { NgModule }                        from '@angular/core';
+import { RouterModule }                    from '@angular/router';
+import { Routes }                          from '@angular/router';
+import 'hammerjs';
+import { FormsModule }                     from '@angular/forms';
+import { ReactiveFormsModule }             from '@angular/forms';
+import { BrowserAnimationsModule }         from '@angular/platform-browser/animations';
+import { MatSelectModule }                 from '@angular/material/select';
+import { MatDialogModule }                 from '@angular/material/dialog';
+import { platformBrowserDynamic }          from '@angular/platform-browser-dynamic';
+import { MatInputModule }                  from '@angular/material';
 
-
-import { AppComponent }    from './app.component';
-import { LoginComponent }  from './authorization/login/login/login.component';
-import { SignupComponent } from './authorization/signup/signup/signup.component';
-
+import { AppRoutingModule }        from './app-routing.module';
+import { AppComponent }            from './app.component';
+import { AuthModule }              from './authorization/auth.module';
+import { AuthService }             from './authorization/auth.service';
+import { LoginComponent }          from './authorization/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    FormsModule,
+    AuthModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDialogModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AuthService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- src/app/authorization/auth.module.ts
+++ src/app/authorization/auth.module.ts
@@ -1,10 +1,29 @@
-import { NgModule }     from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }        from '@angular/core';
+import { CommonModule }    from '@angular/common';
+import { MatButtonModule }                 from '@angular/material';
+import { MatInputModule }                  from '@angular/material';
+import { MatDialogModule }                 from '@angular/material/dialog';
+
+import { LoginComponent }  from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule
   ],
-  declarations: []
+  exports: [
+    MatButtonModule
+  ],
+  declarations: [
+    LoginComponent,
+    SignupComponent
+  ],
+  bootstrap: [
+    LoginComponent
+  ]
 })
 export class AuthModule { }
