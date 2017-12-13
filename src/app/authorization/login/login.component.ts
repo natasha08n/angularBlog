69fbdd6024f09d3259d67fba0c0e88ef3b6c6b28
Natallia Chobat <natallia.chobat@itechart-group.com>
2017-12-13 22:13:09 +0300

    Added code for close dialog modal window

---
+++ src/app/authorization/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit }                        from '@angular/core';
+import { Inject }                                   from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent{
+
+  constructor(
+    public dialogRef: MatDialogRef<LoginComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
