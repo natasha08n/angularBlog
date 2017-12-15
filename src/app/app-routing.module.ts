509b7b4053efe22b752766db133c950e1e1ad659
Natallia Chobat <natallia.chobat@itechart-group.com>
2017-12-15 21:19:33 +0300

    Started work with post-components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule }        from '@angular/core';
-import { CommonModule }    from '@angular/common';
-import { RouterModule }    from '@angular/router';
-import { Routes }          from '@angular/router';
+import { NgModule }            from '@angular/core';
+import { CommonModule }        from '@angular/common';
+import { RouterModule }        from '@angular/router';
+import { Routes }              from '@angular/router';
 
-import { LoginComponent }  from './authorization/login/login.component';
-import { SignupComponent } from './authorization/signup/signup.component';
+import { LoginComponent }      from './authorization/login/login.component';
+import { SignupComponent }     from './authorization/signup/signup.component';
+import { PostCreateComponent } from './post/post-create.component';
 
 const appRoutes: Routes = [
+  //{ path: 'post/:id', component: 'PostDetailComponent' },
+  { path: 'post/create', component: 'PostCreateComponent' }
 ]
 
 @NgModule({
