1bf78735ecac5a68ef9552354774adfc074df5d1
Natallia Chobat <natallia.chobat@itechart-group.com>
2017-12-28 14:06:01 +0300

    Fixed bug with dynamically updated content when toute is changed

--- src/app/post/list-preview/posts-list.component.ts
+++ src/app/post/list-preview/posts-list.component.ts
@@ -1,54 +1,67 @@
-import { Component }            from '@angular/core';
-import { OnInit }               from '@angular/core/src/metadata/lifecycle_hooks';
-import { Subscription }         from 'rxjs/Subscription';
-import { ActivatedRoute }       from '@angular/router';
-import { Subject }              from 'rxjs/Subject';
+import {
+  Component
+} from '@angular/core';
+import {
+  OnInit
+} from '@angular/core/src/metadata/lifecycle_hooks';
+import {
+  Subscription
+} from 'rxjs/Subscription';
+import {
+  ActivatedRoute,
+  Router
+} from '@angular/router';
 
-import { Post }                 from './../../models/post';
-import { PostService }          from  './../post.service';
-import { PostPreviewComponent } from './post-preview.component';
+import {
+  Post
+} from './../../models/post';
+import {
+  PostService
+} from './../post.service';
+import {
+  PostPreviewComponent
+} from './post-preview.component';
 
 
 @Component({
-    selector: 'app-posts-list',
-    templateUrl: './posts-list.component.html',
-    styleUrls: ['./posts-list.component.css']
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css']
 })
 
 export class PostsListComponent implements OnInit {
-    posts: Post[];
-    subscription: Subscription;
+  posts: Post[];
+  subscription: Subscription;
 
-    constructor(
-        private postService: PostService,
-        private route: ActivatedRoute
-    ) {
-        this.subscription = postService.posts$.subscribe(
-            (posts) => this.posts = posts
-           );
-    }
-
-    ngOnInit() {
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.subscription = postService.posts$.subscribe(
+      (posts) => this.posts = posts
+    );
+    router.events.forEach(() => {
         const tagname = this.route.snapshot.paramMap.get('tag');
-        if(tagname) {
-            this.getPostsByTag(tagname);
-        } else {
-            this.getPosts();
-        }
-    }
+      if (tagname) {
+        this.getPostsByTag(tagname);
+      } else {
+        this.getPosts();
+      }
+    });
+  }
 
-    getPosts(): void {
-        this.postService.getAllPosts()
-            .subscribe(posts => {
-                this.posts.next(posts);
-            });
-    }
+  ngOnInit(){
 
-    getPostsByTag(tagname: string): void {
-        this.postService.getPostsByTag(tagname)
-            .subscribe(posts => {
-                this.posts = posts;
-                this.postService.
-            });
-    }
+  }
+
+  getPosts(): void {
+    this.postService.getAllPosts()
+      .subscribe(posts => this.posts = posts);
+  }
+
+  getPostsByTag(tagname: string): void {
+    this.postService.getPostsByTag(tagname)
+      .subscribe(posts => this.posts = posts);
+  }
 }
